41696d7b7f480434020067d383af39421aa64cc7
Jad Sarout <xananax@yelostudio.com>
2018-02-10 18:49:02 +0200

    compose is now proper pipe

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,7 @@ import * as React from 'react'
 import { render } from 'react-dom'
 /**/
 import * as c from './utils'
-const { pipe } = c
-const add = (n: number) => n + 1
-const toStr = (n: number) => 'yey! ' + n
-/**/
 
-const add3 = pipe(toStr, add, add, add)
-
-const App = () => <div>Hello {c + ''} {add3(1)}</div>
+const App = () => <div>Hello {c + ''}</div>
 
 render(<App/>, document.getElementById( 'root' ))
--- src/utils/pipe.ts
+++ src/utils/pipe.ts
@@ -4,12 +4,13 @@ export interface Arity1<A, B>
   { (a: A): B
   }
 
-export function pipe<A, B>(f: Arity1<A, B>): Arity1<A, B>
-export function pipe<A, B, C> (g: Arity1<B, C>, f: Arity1<A, B>): Arity1<A, C>
-export function pipe<A, B, C, D> (h: Arity1<C, D>, g: Arity1<B, C>, f: Arity1<A, B>): Arity1<A, D>
-export function pipe<A, B, C, D, E> (i: Arity1<D, E>, h: Arity1<C, D>, g: Arity1<B, C>, f: Arity1<A, B>): Arity1<A, E>
+export function pipe<A, B>(fn1: Arity1<A, B>): Arity1<A, B>
+export function pipe<A, B, C> (fn1: Arity1<A, B>, fn2: Arity1<B, C>): Arity1<A, C>
+export function pipe<A, B, C, D> (fn1: Arity1<A, B>, fn2: Arity1<B, C>, fn3: Arity1<C, D>): Arity1<A, D>
 // tslint:disable-next-line:max-line-length
-export function pipe<A, B, C, D, E, F> (j: Arity1<E, F>, i: Arity1<D, E>, h: Arity1<C, D>, g: Arity1<B, C>, f: Arity1<A, B>): Arity1<A, F>
+export function pipe<A, B, C, D, E> (fn1: Arity1<A, B>, fn2: Arity1<B, C>, fn3: Arity1<C, D>, fn4: Arity1<D, E>): Arity1<A, E>
+// tslint:disable-next-line:max-line-length
+export function pipe<A, B, C, D, E, F> (fn1: Arity1<A, B>, fn2: Arity1<B, C>, fn3: Arity1<C, D>, fn4: Arity1<D, E>, fn5: Arity1<E, F>): Arity1<A, F>
 // tslint:disable-next-line:no-any
 export function pipe( ...fns: Arity1<any, any>[] ) 
   { switch (fns.length)
@@ -32,37 +33,37 @@ export const pipe1 =
 
 export const pipe2 =
   <A, B, C>
-  ( g: Arity1<B, C>
-  , f: Arity1<A, B>
+  ( g: Arity1<A, B>
+  , f: Arity1<B, C>
   ): Arity1<A, C> =>
-  (a: A): C => g(f(a))
+  (a: A): C => f(g(a))
 
 export const pipe3 = 
   <A, B, C, D> 
-  ( h: Arity1<C, D>
+  ( h: Arity1<A, B>
   , g: Arity1<B, C>
-  , f: Arity1<A, B>
+  , f: Arity1<C, D>
   ): Arity1<A, D> =>
   (a: A): D =>
-  h(g(f(a)))
+  f(g(h(a)))
 
 const pipe4 = 
   <A, B, C, D, E>
-  ( i: Arity1<D, E>
-  , h: Arity1<C, D>
-  , g: Arity1<B, C>
-  , f: Arity1<A, B>
+  ( i: Arity1<A, B>
+  , h: Arity1<B, C>
+  , g: Arity1<C, D>
+  , f: Arity1<D, E>
   ): Arity1<A, E> => 
   (a: A): E =>
-  i(h(g(f(a))))
+  f(g(h(i(a))))
 
 const pipe5 = 
   <A, B, C, D, E, F>
-  ( j: Arity1<E, F>
-  , i: Arity1<D, E>
+  ( j: Arity1<A, B>
+  , i: Arity1<B, C>
   , h: Arity1<C, D>
-  , g: Arity1<B, C>
-  , f: Arity1<A, B>
+  , g: Arity1<D, E>
+  , f: Arity1<E, F>
   ): Arity1<A, F> =>
   (a: A): F =>
-  j(i(h(g(f(a)))));
+  f(g(h(i(j(a)))))
